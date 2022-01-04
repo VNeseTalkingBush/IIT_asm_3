f724b1fbe7cc6c4dfb3dc0d2e0fd7d5a881e7e89
Giang Nguyen <s3914108@rmit.edu.vn>
2022-01-04 12:11:30 +0700

    header + hidden menu

---
+++ js/index.js
@@ -0,0 +1,36 @@
+// back to top
+//Get the button:
+mybutton = document.getElementById("myBtn");
+menu = document.getElementById('dp_menu')
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+	menu.style.display = 'block'
+	} else {
+	menu.style.display = 'none'
+	}
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+// hidden menu
+$(document).ready(function(){
+	$("button").click(function(){
+		$("#dp_menu > ul").toggle(500);
+		$("#dp_menu").toggleClass("show");
+	});
+});
